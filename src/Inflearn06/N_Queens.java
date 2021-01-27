6ad918792a97c0d20e6e6b339d7565be95aba202
qkrthdus605 <qkrthdus605@gmail.com>
2021-01-27 22:32:30 +0900

    N-Queens

---
+++ src/Inflearn06/N_Queens.java
@@ -0,0 +1,22 @@
+package Inflearn06;
+
+public class N_Queens {
+	
+	private static int N = 8;
+	
+	int []cols = new int[N+1];
+	public static boolean queens(int level) {
+		
+	}
+	
+	// 충돌 여부 검사 함수
+	public static boolean promising(int level) {
+		
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
