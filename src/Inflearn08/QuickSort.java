325be3ba472849f8731892e770839c0a279c2d8b
qkrthdus605 <qkrthdus605@gmail.com>
2021-02-03 22:28:05 +0900

    Quick Sort 수도 코드 작성

---
+++ src/Inflearn08/QuickSort.java
@@ -0,0 +1,23 @@
+package Inflearn08;
+
+public class QuickSort {
+	
+	//수도코드
+//	Partition(A, p, r) //수도 코드
+//	{
+//	   x <- A[r]; //pivot
+//	   i <- p-1; //i의 index는 처음부터 시작, 작은 값이 하나도 없는 상태에서 시작이기 때문에 p-1
+//	   for j <- p to r-1 
+//	       if A[j] <= x then //비교한값이 pivot보다 작을 때
+//	          i <- i+1; //작은 애들의 마지막 인덱스를 1증가 
+//	          exchange A[i] and A[j]; //비교한 값을 작은 애들의 마지막으로 넣음
+//	    exchange A[i+1] and A[r]; //맨 끝에 있던 피봇을 큰애들과 작은 애들 사이로 이동
+//	    return i + 1; //pivot의 자리 리턴
+//	}
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
