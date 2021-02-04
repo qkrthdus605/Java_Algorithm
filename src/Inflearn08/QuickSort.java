036a203db662d2c6ac6e9f56eecc702d58d3a16e
qkrthdus605 <qkrthdus605@gmail.com>
2021-02-04 19:11:38 +0900

    QuickSort

--- src/Inflearn08/QuickSort.java
+++ src/Inflearn08/QuickSort.java
@@ -1,5 +1,7 @@
 package Inflearn08;
 
+import java.util.Arrays;
+
 public class QuickSort {
 	
 	//수도코드
@@ -14,10 +16,45 @@ public class QuickSort {
 //	    exchange A[i+1] and A[r]; //맨 끝에 있던 피봇을 큰애들과 작은 애들 사이로 이동
 //	    return i + 1; //pivot의 자리 리턴
 //	}
-	
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public static int partition(int arr[], int left, int right) {
+		// left, right는 배열의 인덱스
+		int pivot = arr[(left + right) / 2]; //배열의 중간을 pivot으로 선정
+
+		while (left < right) {
+			while ((arr[left] < pivot) && (left < right))
+				left++;
+			while ((arr[right] > pivot) && (left < right))
+				right--;
+
+			if (left < right) {
+				int temp = arr[left];
+				arr[left] = arr[right];
+				arr[right] = temp;
+			}
+		}
+
+		return left; //pivot 의 인덱스 리턴 
+	}
+
+	public static void quickSort(int arr[], int left, int right) {
+
+		if (left < right) {
+			int pivotNewIndex = partition(arr, left, right);
+
+			quickSort(arr, left, pivotNewIndex - 1); //왼쪽 정렬
+			quickSort(arr, pivotNewIndex + 1, right); //오른쪽 정렬
+		}
 
 	}
 
+	public static void main(String[] args) {
+		int[] arrs = { 69, 10, 30, 2, 16, 8, 31, 22 };
+		quickSort(arrs, 0, arrs.length - 1);
+		System.out.println("결과");
+
+		for (int i : arrs) {
+			System.out.print(i + " ");
+		}
+	}
 }
+
